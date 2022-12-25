d66f1eebdb47404888e457b24098d45779197830
Aayush Paigwar <90213184+Aayush2711@users.noreply.github.com>
2022-12-25 13:29:30 +0530

    created input list

--- Problem_2.py
+++ Problem_2.py
@@ -1,1 +1,11 @@
-# #Take list of elements from the user and find the square root of each number in the list and store in it another list and print that list.
+# Take list of elements from the user and find the square root of each number in the list and store in it another list and print that list.
+
+
+list=[]
+sqrt=[]
+a= int(input("Enter the numbers:"))
+for i in range (a):
+    num= int(input("Enter the element:"))
+    list.append(num)
+print(list)
+    
