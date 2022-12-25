3a6561764377d4b1ae175dacfbd29fb849d1f79d
Aayush Paigwar <90213184+Aayush2711@users.noreply.github.com>
2022-12-25 13:53:45 +0530

    Done

--- Problem_2.py
+++ Problem_2.py
@@ -1,11 +1,10 @@
 # Take list of elements from the user and find the square root of each number in the list and store in it another list and print that list.
 
-
+import math
 list=[]
-sqrt=[]
 a= int(input("Enter the numbers:"))
 for i in range (a):
     num= int(input("Enter the element:"))
     list.append(num)
-print(list)
-    
+sq= [math.sqrt(i) for i in list]
+print(sq)
