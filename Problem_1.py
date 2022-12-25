9c89c30d0f660a7861945026e18de8b2cd66e39a
Aayush Paigwar <90213184+Aayush2711@users.noreply.github.com>
2022-12-25 13:03:56 +0530

    Done

--- Problem_1.py
+++ Problem_1.py
@@ -1,1 +1,14 @@
-#aayush commit done
+# WAP to accept the two numbers input from the user then add them and print
+
+print("\n Method 1")
+a = input("Enter 1st number")
+b = input("Enter second number")
+c = int(a) + int(b)
+print(c)
+
+print("\n Method 2")
+d = int(input("Enter 1st number"))
+e = int(input("Enter 2nd number"))
+add = d + e
+print(add)
+print(f"The sum of {d}+{e}={add}")
